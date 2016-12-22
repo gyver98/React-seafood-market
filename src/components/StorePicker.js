36f0b6318bf7273a2f6c672cfc50cc22a36961e5
gyver98 <matthewchoi7@gmail.com>
2016-12-22 16:01:46 +1100

    Writing HTML with JSX and comment style

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-
 class StorePicker extends React.Component {
     render() {
+        // javascript style comment
         return (
-            <p>Hello React</p>
+            <form className="store-selector">
+                {/*JSX comment*/}
+                <h2>Please enter a store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store -></button>
+            </form>
         )
     }
 }
