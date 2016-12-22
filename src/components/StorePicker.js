bad906ffe0483549b0cc689095e852b3a8bceb2c
gyver98 <matthewchoi7@gmail.com>
2016-12-22 15:17:36 +1100

    first component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <p>Hello React</p>
+        )
+    }
+}
+
+export default StorePicker;
