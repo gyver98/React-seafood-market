5db64526a7865556978e3d61d39f07bfdc4b5e47
gyver98 <matthewchoi7@gmail.com>
2017-01-10 16:25:50 +1100

    Loading data into state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     //initial state
     this.state = {
       fishes: {},
@@ -14,6 +16,12 @@ class App extends React.Component {
     };
   }
   
+  loadSamples() {
+    this.setState({
+      fishes : sampleFishes
+    });
+  }
+
   addFish(fish) {
     // make a copy from original status
     const fishes = {...this.state.fishes};
@@ -31,7 +39,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
