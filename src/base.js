6e1fabefb342da381ce4c7bec154b013a73aee95
gyver98 <matthewchoi7@gmail.com>
2017-01-11 13:29:45 +1100

    step18. Persisting our State with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDDlpKlO3LhBehKUkbkWsayHZtX8rNn99w",
+  authDomain: "react-seafood-market.firebaseapp.com",
+  databaseURL: "https://react-seafood-market.firebaseio.com"
+});
+
+export default base;
